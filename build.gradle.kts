90b32d48b3f744eb8557da39ccef9781e483aa63
xuke <228619770@qq.com>
2018-11-29 13:27:43 +0800

    添加游戏引擎依赖包

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
+allprojects {
+    repositories {
+        maven {
+            setUrl("https://jitpack.io")
+        }
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.3.10"
 }
@@ -9,11 +18,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        setUrl("https://jitpack.io")
+    }
 }
 
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
-}
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.github.shaunxiao:kotlinGameEngine:v0.1.0")}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
---
+++ src/test/kotlin/MyWindow.kt
@@ -0,0 +1,22 @@
+import javafx.application.Application
+import javafx.scene.input.KeyEvent
+import org.itheima.kotlin.game.core.Window
+
+class MyWindow : Window() {
+    override fun onCreate() {
+    }
+
+    override fun onDisplay() {
+    }
+
+    override fun onKeyPressed(event: KeyEvent) {
+    }
+
+    override fun onRefresh() {
+    }
+}
+
+
+fun main(args: Array<String>) {
+    Application.launch(MyWindow::class.java)
+}
